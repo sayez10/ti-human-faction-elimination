4c6356061d63efed4a367bcaf2d025f7346b5bee
sayez10 <227934377+sayez10@users.noreply.github.com>
2025-10-08 19:02:25 +0200

    Change all access modifiers in Main.cs to the minimum necessary

--- TIHumanFactionEliminationMod/Main.cs
+++ TIHumanFactionEliminationMod/Main.cs
@@ -16,17 +16,17 @@ namespace TIHumanFactionEliminationMod
     /// <summary>
     /// Controls loading and managing the mod
     /// </summary>
-    public class Main
+    internal class Main
     {
-        public static bool enabled;
-        public static ModEntry mod;
+        private static ModEntry mod;
+        internal static bool enabled;
 
         /// <summary>
         /// Entry point of the application (as per ModInfo.json), which applies the Harmony patches
         /// </summary>
         /// <param name="modEntry"></param>
         /// <returns></returns>
-        static bool Load(ModEntry modEntry)
+        private static bool Load(ModEntry modEntry)
         {
             var harmony = new Harmony(modEntry.Info.Id);
             mod = modEntry;
@@ -41,7 +41,7 @@ namespace TIHumanFactionEliminationMod
         /// <param name="modEntry"></param>
         /// <param name="value"></param>
         /// <returns></returns>
-        static bool OnToggle(ModEntry modEntry, bool value)
+        private static bool OnToggle(ModEntry modEntry, bool value)
         {
             enabled = value;
             return true;
